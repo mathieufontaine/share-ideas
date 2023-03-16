cca217fee1877ce7198029f87154b576beef4671
mathieufontaine <mathieufontaine.dev@gmail.com>
2023-03-16 16:57:19 +0100

    changed port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const ideaRouter = require("./routes/ideas");
 const cors = require("cors");
 require("dotenv").config();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 const connectDB = require("./config/db");
 
 connectDB();
